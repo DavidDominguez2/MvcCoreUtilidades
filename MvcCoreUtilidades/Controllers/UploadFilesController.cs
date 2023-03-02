a218b7cede9bb246591ed72ca6b99e9715afe7bc
David Domínguez Blanco <david.dominguez.blanco.2001@gmail.com>
2023-03-02 14:15:27 +0100

    finish fichero

---
+++ MvcCoreUtilidades/Controllers/UploadFilesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreUtilidades.Helpers;
+using static MvcCoreUtilidades.Helpers.HelperPathProvider;
+
+namespace MvcCoreUtilidades.Controllers {
+    public class UploadFilesController : Controller {
+
+
+        private HelperPathProvider helperPathProvider;
+
+        public UploadFilesController(HelperPathProvider helperPathProvider) {
+
+            this.helperPathProvider = helperPathProvider;
+        }
+
+        public IActionResult SubirFicheros() {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SubirFicheros(IFormFile fichero) {
+            string fileName = fichero.FileName;
+            string path = this.helperPathProvider.MapPath(fileName, Folders.Uploads);
+
+            using (Stream stream = new FileStream(path, FileMode.Create)) {
+                await fichero.CopyToAsync(stream);
+            }
+
+            ViewData["MENSAJE"] = "Fichero subido a " + path;
+            return View();
+        }
+    }
+}
--- MvcCoreUtilidades/Program.cs
+++ MvcCoreUtilidades/Program.cs
@@ -1,6 +1,9 @@
+using MvcCoreUtilidades.Helpers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSingleton<HelperPathProvider>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
